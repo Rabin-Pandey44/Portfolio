84a85b7b4d67d0d7255b30c91bf690287ec58755
Rabin Pandey <pandeyrabin27@gmail.com>
2024-08-08 09:32:05 +0545

    all change

--- project.js
+++ project.js
@@ -1,5 +1,4 @@
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Thank you for your message! I will get back to you soon.');
-    document.getElementById('contactForm').reset();
 });
